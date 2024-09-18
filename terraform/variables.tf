30188a5e6606c07da29b1f82c8035f4e148db713
JoseVitorDurante <josevitor_limma@hotmail.com>
2024-09-17 21:09:14 -0300

    Criando repo

---
+++ terraform/variables.tf
@@ -0,0 +1,29 @@
+variable "region" {
+  description = "AWS region to deploy to"
+  default     = "us-east-1"
+  type        = string
+}
+
+variable "availability_zones" {
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+  description = "List of availability zones for the selected region"
+}
+
+variable "database_credentials" {
+  description = "Credentials for database creation"
+
+  type = object({
+    username = string
+    password = string
+    port     = string
+    name     = string
+  })
+
+  default = {
+    username = "postgres"
+    password = "postgres"
+    port     = 5432
+    name     = "apifood"
+  }
+}
