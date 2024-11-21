758943d59b30dcd0ef28ff25f0ed643fcbf5939a
JoseVitorDurante <josevitor_limma@hotmail.com>
2024-11-21 13:18:56 -0300

    separando para outro repo o mongobd

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,20 +2,11 @@ provider "aws" {
   region = var.region
 }
 
-provider "mongodbatlas" {
-  public_key  = var.api_payments_database_credentials.atlas_public_key
-  private_key = var.api_payments_database_credentials.atlas_private_key
-}
-
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.48"
-    }
-    mongodbatlas = {
-      source  = "mongodb/mongodbatlas"
-      version = "1.4.5"
     }
   }
 
